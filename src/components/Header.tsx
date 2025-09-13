7dde4debf7029b16ab308f4af2d6e166a9da9536
eduard0vieira <eduardohvieira05@gmail.com>
2025-09-12 22:03:12 -0300

    Refactor Header component: restore ReactIcon link and adjust layout for improved accessibility and structure.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,11 @@ import ReactIcon from "./icons/ReactIcon";
 
 export default function Header() {
   return (
-    <header className="flex justify-between h-12 p-5 bg-white/10 backdrop-blur-md border border-white/20 shadow-xl shadow-black/10 my-5 rounded-xl">
-      <div className="flex items-center justify-center">
-        <ReactIcon className="animate-pulse"/>
+    <header className="flex justify-between items-center h-12 p-5 bg-white/10 backdrop-blur-md border border-white/20 shadow-xl shadow-black/10 my-5 rounded-xl">
+      <a href="/"><ReactIcon className="animate-pulse" /></a>
+      <div className="text-white text-1xl ml-2 flex justify-end gap-4">
+        <a href="/register">Register</a>
+        <a href="/login">Login</a>
       </div>
     </header>
   )
