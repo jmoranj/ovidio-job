a81888e938a5cc4bfa547b9575a148dc98936041
eduard0vieira <eduardohvieira05@gmail.com>
2025-09-17 12:12:57 -0300

    Update animation properties in Title component: adjust initial positions and transition durations for smoother visual effects.

--- src/components/title/Title.tsx
+++ src/components/title/Title.tsx
@@ -9,24 +9,24 @@ export default function Title() {
       transition={{ duration: 3.5, ease: "easeOut" }}
       className="flex flex-col gap-10"
     >
-      <motion.h1 
-      initial={{ x: 200 }}
-      animate={{ x: 0 }}
-      transition={{ duration: 1, ease: "easeOut" }}
-      className="flex font-bold text-9xl bg-clip-text text-transparent bg-gradient-to-r from-cyan-50 to-cyan-500">
+      <motion.h1
+        initial={{ x: 80 }}
+        animate={{ x: 0 }}
+        transition={{ duration: 2, ease: "easeOut" }}
+        className="flex font-bold text-9xl bg-clip-text text-transparent bg-gradient-to-r from-cyan-50 to-cyan-500">
         Olá, <br /> Bem vindo
       </motion.h1>
 
-      <p className='text-white flex flex-col text md:text-5xl text-4xl'>
-        <motion.span 
-        initial={{ x: 300 }}
-        animate={{ x: 0 }}
-        transition={{ duration: 2, ease: "easeOut" }}>Projeto desenvolvido por</motion.span>
+      <p className='text-white flex flex-col text md:text-5xl text-4xl gap-2'>
+        <motion.span
+          initial={{ x: 80 }}
+          animate={{ x: 0 }}
+          transition={{ duration: 2, ease: "easeOut" }}>Projeto desenvolvido por</motion.span>
 
-        <motion.span 
-        initial={{ y: 300 }}
-        animate={{ y: 0 }}
-        transition={{ duration: 2, ease: "easeOut" }}>@jmoranj e @eduard0vieira, estudantes da UNISO.</motion.span>
+        <motion.span
+          initial={{ opacity: 0, y: 60 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 2, ease: "easeOut" }}>@jmoranj e @eduard0vieira, estudantes da UNISO.</motion.span>
       </p>
     </motion.div>
   )
