1580672ba77c175bb2589d5dc4e97e003749be86
jmoranj <morandim.joao@gmail.com>
2025-09-16 23:25:01 -0300

    Refactor SubmitButton component: enhance props structure by adding text prop for button label and streamline button attributes for better flexibility.

--- src/components/form/SubmitButton.tsx
+++ src/components/form/SubmitButton.tsx
@@ -1,12 +1,18 @@
+interface SubmitButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  text: string;
+}
+
 export default function SubmitButton({
-  name
-}: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+  text,
+  ...props
+}: SubmitButtonProps) {
   return (
     <button
       type="submit"
       className="text-black rounded-lg bg-[#D0D1D1] ring-1 ring-gray-700 px-4 py-2 font-semibold hover:shadow-[0_0_200px_#61DAFB] transition-all duration-300 disabled:opacity-50"
+      {...props}
     >
-      {name}
+      {text}
     </button>
   );
 }
