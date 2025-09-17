564d14cc8a91306c15bc0b0536eab0ee6721def3
jmoranj <morandim.joao@gmail.com>
2025-09-17 20:05:24 -0300

    feat: implement password confirmation in RegisterForm component
    Added state management for password and confirmation fields, including validation to ensure both passwords match before form submission. Display error message if passwords do not match.

--- src/components/form/RegisterForm.tsx
+++ src/components/form/RegisterForm.tsx
@@ -1,21 +1,45 @@
+import { useState } from "react";
 import InputField from "./InputField";
 import SubmitButton from "./SubmitButton";
 
 export default function FormComponent() {
+  const [password, setPassword] = useState("");
+  const [isError, setIsError] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+
+    if (password !== confirmPassword) {
+      setIsError("As senhas não coincidem");
+      return;
+    }
+
     window.location.href = "/login";
   };
 
   return (
     <form className="flex w-full max-w-md flex-col gap-4" onSubmit={handleSubmit}>
       <InputField label="Name" name="name" type="text" required />
-      <InputField label="Email" name="email" type="email" required />
-      <InputField label="Password" name="password" type="password" required />
-      <InputField label="Confirm Password" name="confirmPassword" type="password" required />
 
-      <SubmitButton text="Enviar"/>
+      <InputField label="Email" name="email" type="email" required />
+
+      <InputField 
+      label="Password" 
+      name="password" 
+      type="password" 
+      required value={password} 
+      onChange={(e) => setPassword(e.target.value)} />
+
+      <InputField 
+      label="Confirm Password" 
+      name="confirmPassword" 
+      type="password" 
+      required value={confirmPassword} 
+      onChange={(e) => setConfirmPassword(e.target.value)} />
+
+      {isError && <p className="text-red-500">{isError}</p>}
+      <SubmitButton text="Enviar" />
     </form>
   );
 }
